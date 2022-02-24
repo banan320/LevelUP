a459deb97d60b44593f0823f428e49301e4ceb6d
Alex Gerber <alex-gerber@mail.ru>
2022-02-24 19:10:28 +0300

    full edits

--- JS/script.js
+++ JS/script.js
@@ -31,6 +31,14 @@ $(function () {
   }, 1000);
 });
 
+// прокрутка к форме заказа
+$(document).on("click", ".btn__order", function (e) {
+  e.preventDefault();
+  var id = $(this).attr("href");
+  var top = $(id).offset().top; // получаем координаты блока
+  $("body, html").animate({ scrollTop: top }, 800); // плавно переходим к блоку
+});
+
 function ibg() {
   $.each($(".ibg"), function (index, val) {
     if ($(this).find("img").length > 0) {
@@ -43,4 +51,3 @@ function ibg() {
 }
 
 ibg();
-
